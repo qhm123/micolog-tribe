857970dd0d02b44e3accc9167af13d1da1371511
qhm123 <qhm123@gmail.com>
2011-01-28 18:01:32 +0800

    添加ping service 服务。具体ping操作尚未实现。

--- tools/urls.py
+++ tools/urls.py
@@ -4,4 +4,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('tools.views',
     (r'^hosts$', 'hosts'),
+    (r'^rpc_handler', 'rpc_handler'),
 )
--- tools/views.py
+++ tools/views.py
@@ -1,9 +1,12 @@
 # coding: utf-8
 
+from SimpleXMLRPCServer import SimpleXMLRPCDispatcher
+
 from django import http
 from django.template import RequestContext, loader
 
 from common.models import Blog
+import logging
 
 IP = '203.208.39.104'
 
@@ -18,3 +21,44 @@ def hosts(request):
     })
     
     return http.HttpResponse(template.render(context))
+
+def rpc_handler(request):
+    
+    if len(request.POST):
+        response = http.HttpResponse(mimetype="application/xml")
+        response.write(server._marshaled_dispatch(request.raw_post_data))
+    else:
+        response = http.HttpResponse()
+        response.write("<b>This is an XML-RPC Service.</b><br>")
+        response.write("You need to invoke it using an XML-RPC Client!<br>")
+        
+#        response.write("The following methods are available:<ul>")
+#        methods = server.system_listMethods()
+#
+#        for method in methods:
+#            # right now, my version of SimpleXMLRPCDispatcher always
+#            # returns "signatures not supported"... :(
+#            # but, in an ideal world it will tell users what args are expected
+#            sig = server.system_methodSignature(method)
+#
+#            # this just reads your docblock, so fill it in!
+##            help =  server.system_methodHelp(method)
+#            help = ''
+#
+#            response.write("<li><b>%s</b>: [%s] %s" % (method, sig, help))
+#
+#        response.write("</ul>")
+#        response.write('<a href="http://www.djangoproject.com/"> <img src="http://media.djangoproject.com/img/badges/djangomade124x25_grey.gif" border="0" alt="Made with Django." title="Made with Django."></a>')
+#
+#    response['Content-length'] = str(len(response.content))
+    return response
+
+server = SimpleXMLRPCDispatcher()
+server.register_introspection_functions()
+        
+def ping(name, site_url, entry_url, rss_url):
+    logging.info('ping %s %s %s %s' % (name, site_url, entry_url, rss_url))
+    return 'ping %s %s %s %s' % (name, site_url, entry_url, rss_url)
+
+server.register_function(ping, 'weblogUpdates.ping')
+
