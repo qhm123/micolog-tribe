9f08b7e1a2aaeede2c86a1e4ac393ebc17d328aa
qhm123 <qhm123@gmail.com>
2011-02-27 19:03:55 +0800

    fixed the rssa entry date.

--- rssa/feedfetch.py
+++ rssa/feedfetch.py
@@ -1,4 +1,9 @@
+# coding: utf-8
+
+from datetime import datetime
+
 import feedparser
+
 
 class FeedParser():
     
@@ -13,7 +18,7 @@ class FeedParser():
         self.entries = (Entry(entry.id,
                               entry.title,
                               entry.link,
-                              entry.updated_parsed,
+                              entry.updated,
                               entry.description,
                               entry.content[0].value) for entry in feed.entries)
             
@@ -23,6 +28,22 @@ class Entry():
         self.id = id;
         self.title = title
         self.link = link
-        self.updated = updated
+        #Tue, 09 Nov 2010 08:33:11 +0000
+        self.updated = datetime.strptime(updated, '%a, %d %b %Y %H:%M:%S +0000')
         self.description = description
         self.content = content
+        
+if __name__ == "__main__":
+    feed = FeedParser("http://www.qhm123.com/feed")
+    print feed.title
+    print feed.subtitle
+    print feed.link
+    print feed.entries
+    for entry in feed.entries:
+        print entry.id
+        print entry.title
+        print entry.link
+        print entry.updated
+        print entry.description
+        print entry.content
+    
